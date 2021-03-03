7043500d4df681ddeeadbaab959284360423e5b7
Byron <cm9930914@gmail.com>
2021-03-03 01:44:47 -0800

    add main.cpp

--- mbed-os-test-program/main.cpp
+++ mbed-os-test-program/main.cpp
@@ -1,7 +1,7 @@
 #include "mbed.h"
 
 
-DigitalOut myled(LED2);
+DigitalOut myled(LED3);
 
 DigitalOut myled2(LED1);
 
@@ -19,11 +19,63 @@ int main()
       myled2 = 0; // set LED1 pin to low
 
       ThisThread::sleep_for(1s);
-
+      
 
       myled.write(0);  // set LED2 pin to low
 
-      myled2.write(1); // set LED1 pin to high
+      myled2.write(0); // set LED1 pin to high
+
+      ThisThread::sleep_for(1s);
+
+      myled = 1;  // set LED2 pin to high
+
+      myled2 = 0; // set LED1 pin to low
+
+      ThisThread::sleep_for(1s);
+      
+
+      myled.write(0);  // set LED2 pin to low
+
+      myled2.write(0); // set LED1 pin to high
+
+      ThisThread::sleep_for(1s);
+
+      myled = 1;  // set LED2 pin to high
+
+      myled2 = 0; // set LED1 pin to low
+
+      ThisThread::sleep_for(1s);
+      
+
+      myled.write(0);  // set LED2 pin to low
+
+      myled2.write(0); // set LED1 pin to high
+
+      ThisThread::sleep_for(1s);
+
+      myled = 0;  // set LED2 pin to high
+
+      myled2 = 1; // set LED1 pin to low
+
+      ThisThread::sleep_for(1s);
+      
+
+      myled.write(0);  // set LED2 pin to low
+
+      myled2.write(0); // set LED1 pin to high
+
+      ThisThread::sleep_for(1s);
+
+      myled = 0;  // set LED2 pin to high
+
+      myled2 = 1; // set LED1 pin to low
+
+      ThisThread::sleep_for(1s);
+      
+
+      myled.write(0);  // set LED2 pin to low
+
+      myled2.write(0); // set LED1 pin to high
 
       ThisThread::sleep_for(1s);
 
